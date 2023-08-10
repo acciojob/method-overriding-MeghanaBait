e764148e6229f9133f0b72fdbbc457fb5cbcb7ea
Meghana Bait <meghanawork777@gmail.com>
2023-08-10 19:30:12 +0530

    Adding Solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,30 @@
 package com.driver;
 
 public class Main {
-  
+    public static class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+
+    public static class B extends A{
+        @Override
+        public String meth(){
+            return "Method is overridden in Extended class B";
+        }
+
+        public String callSuperMeth() {
+            // Using super to call the method from the parent class (class A)
+            return super.meth();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        B obj1 = new B();
+        obj1.callSuperMeth();
+
+        B obj2 = new B();
+        obj2.meth();
+    }
 }
